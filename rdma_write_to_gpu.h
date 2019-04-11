464eaeeb531b90d912730eef9424985c5490a295
Michael Berezin <michaelbe@mellanox.com>
2019-04-11 15:38:36 +0300

    New interfaces

---
+++ rdma_write_to_gpu.h
@@ -0,0 +1,139 @@
+/*
+ * Copyright (c) 2019 Mellanox Technologies, Inc.  All rights reserved.
+ *
+ * This software is available to you under a choice of one of two
+ * licenses.  You may choose to be licensed under the terms of the GNU
+ * General Public License (GPL) Version 2, available from the file
+ * COPYING in the main directory of this source tree, or the
+ * OpenIB.org BSD license below:
+ *
+ *     Redistribution and use in source and binary forms, with or
+ *     without modification, are permitted provided that the following
+ *     conditions are met:
+ *
+ *      - Redistributions of source code must retain the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer.
+ *
+ *      - Redistributions in binary form must reproduce the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef _RDMA_WRITE_TO_GPU_H_
+#define _RDMA_WRITE_TO_GPU_H_
+
+/* This file defines `struct iovec'  */
+#include <bits/uio.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+ * rdma_device object holds the RDMA resources of the local RDMA device, 
+ * of a Targte or a Source
+ */
+struct rdma_device;
+
+/*
+ * rdma_buffer is used to represent the rdma parameters of a
+ * applciation buffer on a specific device to be used in the RDMA operations
+ */
+struct rdma_buffer;
+
+/* 
+ * Open a RDMA device and allocated requiered resources.
+ * find the capable RDMA device based on the 'addr' as an ip address
+ * of the RDMA device selected to preform the RDMA operations.
+ * Creates a PD, CQ, and QP as internal HW resources.
+ *
+ * Source rdma_device preforms the RDMA Write operation to 
+ * the Target rdma_device.
+ *
+ * returns: a pointer to a rdma_device object or NULL on error
+ */
+struct rdma_device *rdma_open_device_target(struct sockaddr *addr);
+struct rdma_device *rdma_open_device_source(struct sockaddr *addr);
+
+/* 
+ * Close and release all rdma_device resources
+ */
+void rdma_close_device(struct rdma_device *device);
+
+/*
+ * register and deregister an applciation buffer with the RDMA device
+ */
+struct rdma_buffer *rdma_buffer_reg(struct rdma_device device, void *addr, size_t length);
+void rdma_buffer_dereg(struct rdma_buffer *buffer);
+
+/* 
+ * Get a rdma_buffer addr string representation
+ * 
+ * The Target application should pass this address string to the 
+ * Source which will issue the RDMA Write operation
+ *
+ * addr is input and output holding the rdma_buffer information
+ * length is the size of the allocated addr
+ * 
+ * returns: an integer equal to the size of the data copied into addr
+ */
+int rdma_buffer_get_addr_str(struct rdma_buffer *buffer, char *buffer_addr, size_t length);
+
+struct rdma_write_attr {
+	char               *remote_buffer_addr_str;
+	struct rdma_buffer *local_buf_rdma;
+	struct iovec       *local_buf_iovec;
+	int                 local_buf_iovcnt;
+	uint64_t            wr_id;
+};
+
+/*
+ * Issue a RDMA Write operation from the rdma_device, to a remote buffer which
+ * is descibed by the remote_buffer_addr_str.
+ * The local_iov gather list, of size local_iovcnt, hold the buffer addr & size
+ * pairs, and should be in the range of the local_buffer, which holds relevant
+ * the rdma info
+ *
+ * On completion of the RDMA operation, the status and wr_id will be reported
+ * from rdma_poll_completions()
+ *
+ * returns: 0 on success, or the value of errno on failure
+ */
+int rdma_write_to_peer(struct rdma_device *device, struct rdma_write_attr *attr);
+
+enum rdma_completion_status {
+	RDMA_STATUS_SUCCESS,
+	RDMA_STATUS_ERR_LAST,
+};
+
+struct rdma_completion_event {
+	uint64_t                    wr_id;
+	enum rdma_completion_status status;
+};
+
+/*
+ * Return rdma operations which have completed.
+ * the event will hold the requets id (wr_id) and the status of the operation.
+ *
+ * returns: number of reported events in the event array (<= num_entries)
+ */
+int rdma_poll_completions(struct rdma_device *device,
+		struct rdma_completion_event *event,
+		uint32_t num_entries);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _RDMA_WRITE_TO_GPU_H_ */
